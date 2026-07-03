675fa01b3bfdd3658f55219b1b522bc34a6c8e0f
Hema Satyanarayana <hema.satyanarayana@apxor.com>
2026-07-03 19:20:00 +0530

    task: ex-1 completed

--- go-lab/exercises/01-fundamentals/fundamentals.go
+++ go-lab/exercises/01-fundamentals/fundamentals.go
@@ -3,6 +3,7 @@ package fundamentals
 
 import (
 	"errors"
+	"strconv"
 )
 
 // ErrEmpty is returned by Max when called with no arguments.
@@ -15,27 +16,67 @@ var ErrEmpty = errors.New("fundamentals: no values provided")
 //   - "FizzBuzz" if divisible by both
 //   - the decimal number otherwise (e.g. "7")
 func FizzBuzz(n int) []string {
-	panic("TODO: implement FizzBuzz")
+	// panic("TODO: implement FizzBuzz")
+	var result []string
 
+	for i := 1; i <= n; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			result = append(result, "FizzBuzz")
+		case i%3 == 0:
+			result = append(result, "Fizz")
+		case i%5 == 0:
+			result = append(result, "Buzz")
+		default:
+			result = append(result, strconv.Itoa(i))
+		}
+	}
+
+	return result
 }
 
 // Max returns the largest of the provided values. If no values are given it
 // returns ErrEmpty. Use the variadic signature and a range loop.
 func Max(nums ...int) (int, error) {
-	panic("TODO: implement Max")
+	// panic("TODO: implement Max")
+	if len(nums) == 0 {
+		return 0, ErrEmpty
+	}
 
+	var max int
+	max = nums[0]
+	for _, n := range nums {
+		if n > max {
+			max = n
+		}
+	}
+	return max, nil
 }
 
 // IsPrime reports whether n is a prime number. Numbers < 2 are not prime.
 // Aim for an O(sqrt(n)) trial-division loop.
 func IsPrime(n int) bool {
-	panic("TODO: implement IsPrime")
+	// panic("TODO: implement IsPrime")
+	if n < 2 {
+		return false
+	}
 
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
 }
 
 // GCD returns the greatest common divisor of a and b using Euclid's algorithm.
 // GCD(0, 0) is defined here as 0.
 func GCD(a, b int) int {
-	panic("TODO: implement GCD")
+	// panic("TODO: implement GCD")
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
 
 }
